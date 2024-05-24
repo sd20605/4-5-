6d104835ff68ecfd4f494a00fd453e8d199b695d
knutwon <>
2024-05-24 16:48:16 +0900

    dfsf
    github desk

---
+++ file123.py
@@ -0,0 +1,11 @@
+
+class Bun:
+  def__init__(self,name,price2)
+  self.contents=name
+  self.price=price2
+   def sell(self):
+       print(self.contents+"붕어빵이 팬매되었습니다.")
+   cream_bun=Bun("크림",1000)
+# print(cream_bun.contents,cream_bun.price)
+
+cream_bun.sell()
