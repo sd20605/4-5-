15861fee368f99313ef67417b91915d337348158
knutwon <>
2024-05-24 16:58:34 +0900

    fsdff
    dfdsfd

--- rera.py
+++ rera.py
@@ -1,12 +1,18 @@
 
 
 class Bun:
-  def__init__(self,name,price2)
-  self.contents=name
-  self.price=price2
-   def sell(self):
-       print(self.contents+"붕어빵이 팬매되었습니다.")
-   cream_bun=Bun("크림",1000)
+    def __init__(self,name,price2):
+        self.contents=name
+        self.price=price2
+        self.total=0
+
+
+    def sell(self):
+        self.total+=self.price
+        print(self.contents+"붕어빵이 팬매되었습니다.")
+cream_bun=Bun("크림",1000)
 # print(cream_bun.contents,cream_bun.price)
 
 cream_bun.sell()
+cream_bun.sell()
+print("누적 판매금은",cream_bun.total, "원입니다")
